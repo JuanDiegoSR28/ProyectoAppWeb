730aa54ce657e00e2e93dc9d40c27424d595d455
Daniel Alameda <daoma222@gmail.com>
2024-04-28 02:14:26 -0700

    Ajustes

--- ProyectoAppWeb/src/main/java/mx/itson/proyectoappweb/modelo/dominio/Credencial.java
+++ ProyectoAppWeb/src/main/java/mx/itson/proyectoappweb/modelo/dominio/Credencial.java
@@ -4,10 +4,88 @@
  */
 package mx.itson.proyectoappweb.modelo.dominio;
 
-/**
- *
- * @author User
- */
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "credenciales")
 public class Credencial {
-    
+
+    //Atributos
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_credencial;
+
+    @Column(name = "correo", nullable = false, unique = true, length = 300)
+    private String correo;
+
+    @Column(name = "contrasenia", nullable = false, length = 50)
+    private String contrasenia;
+
+    public Credencial() {
+    }
+
+    public Credencial(String correo, String contrasenia) {
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+    }
+
+    public Credencial(Long id_credencial, String correo, String contrasenia) {
+        this.id_credencial = id_credencial;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+    }
+
+    public Long getId_credencial() {
+        return id_credencial;
+    }
+
+    public void setId_credencial(Long id_credencial) {
+        this.id_credencial = id_credencial;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.id_credencial);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Credencial other = (Credencial) obj;
+        return Objects.equals(this.id_credencial, other.id_credencial);
+    }
+
 }
--- ProyectoAppWeb/src/main/java/mx/itson/proyectoappweb/servlet/SrvLogin.java
+++ ProyectoAppWeb/src/main/java/mx/itson/proyectoappweb/servlet/SrvLogin.java
@@ -36,7 +36,7 @@ public class SrvLogin extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet SrvLogin</title>");            
+            out.println("<title>Este es un Get</title>");            
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet SrvLogin at " + request.getContextPath() + "</h1>");
@@ -45,6 +45,23 @@ public class SrvLogin extends HttpServlet {
         }
     }
 
+    
+        protected void processRequestPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Este es un post</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SrvLogin at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -71,17 +88,7 @@ public class SrvLogin extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        processRequestPost(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
